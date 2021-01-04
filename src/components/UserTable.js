c661bb9e244ef03a42897b4db8c3bfdd1e0365fc
davidg <davidgong99@gmail.com>
2021-01-04 23:51:44 +1100

    fetch user data from backend

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -8,41 +8,77 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
   
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
+// function createData(name, calories, fat, carbs, protein) {
+//     return { name, calories, fat, carbs, protein };
+// }
   
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// const rows = [
+//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//     createData('Eclair', 262, 16.0, 24, 6.0),
+//     createData('Cupcake', 305, 3.7, 67, 4.3),
+//     createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
 
 class UserTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            users: [],
+        }
+    }
+    
+    componentDidMount() {
+        fetch("http://localhost:8080/user")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        users: result
+                    });
+                },
+                // Handle errors here to catch issues with components
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+            
+    }
 
-    render () {
-        return (
-            <TableContainer component={Paper}>
-              <Table styles={{ minWidth: "650" }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">User list</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow key={row.name}>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          );
+    render() {
+        const { error, isLoaded, items } = this.state;
+        
+        if (error) {
+            return <div>Error: {error.message} </div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <TableContainer component={Paper}>
+                  <Table styles={{ minWidth: "650" }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="center">User list</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {items.map((row) => (
+                        <TableRow key={row.name}>
+                          <TableCell component="th" scope="row">
+                            {row.name}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+            );
+        }
     }
 }
 
