964aee70a99b70e5994982ed4c574d8fde30504c
Stephanie <s.kan@student.unsw.edu.au>
2021-01-04 23:49:09 +1100

    implement user end point

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+const JSONparser = bodyParser.json();
+
 var users = [];
 
 app.get('/ping', function (req,res) {
@@ -18,17 +20,39 @@ app.get('/user', function (req,res) {
     return res.send(JSON.stringify(users));
 })
 
-app.put('/user', function (req,res){
-    return res.send('update user')
+app.put('/user', JSONparser, function (req,res){
+    if (!find(req.body.oldName)){
+        throw new Error(oldName + ' not found in user list');
+    }
+        for (var i in users) {
+            if (users[i] == req.body.oldName) {
+                users[i] = req.body.newName;
+                return res.send(req.body.oldName + ' updated to ' + req.body.newName);
+            }
+        }
 })
 
-app.post('/user', function (req,res) {
-    return res.send('add user');
+app.post('/user', JSONparser, function (req,res) {
+    if (find(req.body.name)) {
+        throw new Error('User already exists');
+    }
+    users.push(req.body.name);
+    return res.send('added user: '+ req.body.name);
+    
 })
 
 app.delete('/user', function (req,res) {
-    return res.send('delete user')
+    return res.send('delete user');
 })
+
+function find(user) {
+    for (var i in users) {
+        if (users[i] == user) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 app.listen(process.envPORT || 8080);
