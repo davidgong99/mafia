9fc3a6cdffb12c546b471dd993e3b01b725b0106
Stephanie <s.kan@student.unsw.edu.au>
2021-01-04 22:08:16 +1100

    adding basic routes

--- server.js
+++ server.js
@@ -12,5 +12,24 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
+//adds user to list of players
+app.get('/user', function (req,res) {
+    return res.send('add user');
+})
+
+//return user's individual role
+app.get('/getrole', function (req,res) {
+    return res.send('your role');
+})
+
+//return list of mafia
+app.get('/mafia', function (req,res){
+    return res.send('mafia');
+})
+
+//returns list of civilians
+app.get('/civilians', function (req,res) {
+    return res.send('civilians');
+})
 
 app.listen(process.envPORT || 8080);
