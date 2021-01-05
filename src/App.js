735a9964b29909d81e78bf1c300841599a62ccb1
davidg <davidgong99@gmail.com>
2021-01-05 23:11:16 +1100

    add username form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 // import logo from './logo.svg';
 import './App.css';
 import UserTable from './components/UserTable'
+import LoginPage from './components/LoginPage'
+
+import Button from '@material-ui/core/Button'
 
 function App() {
 
   return (
     <div className="App">
+      <div style={{ padding: "10%" }}>
+        Mafia game
+
+        <div  style={{ "borderStyle": "dotted" }}>
+          <h2>User list table</h2>
+          <UserTable/>
+        </div>
+        
+        
+        {/* Buttons */}
+        <div  style={{ "borderStyle": "dotted" }}>
+          <h2>Login page</h2>
+          <LoginPage/>
+        </div>
+        <br/>
+        <Button variant="contained">I am ready</Button>
+        <br/>
+        <Button variant="contained">Start game</Button>
+        
+      </div>
       
-      Mafia game
-      <UserTable/>
     </div>
   );
 }
---
+++ src/components/LoginPage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+  
+// function createData(name, calories, fat, carbs, protein) {
+//     return { name, calories, fat, carbs, protein };
+// }
+  
+// const rows = [
+//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//     createData('Eclair', 262, 16.0, 24, 6.0),
+//     createData('Cupcake', 305, 3.7, 67, 4.3),
+//     createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          username: null,
+          // error: false
+        }
+    }
+    
+    
+    // When username is updated, update state
+    usernameOnChange = (event) => {
+    
+      this.setState({
+        username: event.target.value,
+      });
+    }
+    
+    // When username is submitted, check if username is valid
+    // username can only have letters and numbers
+    // max 15 chars
+    usernameOnSubmit = (event) => {
+      event.preventDefault();
+      
+      // var username = this.state.username;
+      if (this.state.username.match(/^[A-Za-z]{1,15}$/g)) { // valid match
+        console.log("username is valid");
+        console.log("submitting username = ", this.state.username);
+      } else {
+        // send post request
+        console.log("invalid username");
+      }
+      
+    }
+
+    render() {        
+      return (
+        <div>
+          <form autoComplete="off" onSubmit={this.usernameOnSubmit}>
+            <TextField id="outlined-basic" label="Username" variant="outlined" onChange={this.usernameOnChange}/>
+          </form>
+          <Button variant="contained">Login</Button>
+        </div>
+        
+      );
+  
+    }
+}
+
+export default LoginPage;
